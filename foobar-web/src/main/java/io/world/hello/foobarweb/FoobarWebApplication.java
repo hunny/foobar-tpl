49e1c7611d854d2b46cec3e908bdf564bfa66e9b
Huzexiong <huzexiong@hd123.com>
2018-11-19 13:50:30 +0800

    Multi modules template.

---
+++ foobar-web/src/main/java/io/world/hello/foobarweb/FoobarWebApplication.java
@@ -0,0 +1,12 @@
+package io.world.hello.foobarweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobarWebApplication.class, args);
+	}
+}
